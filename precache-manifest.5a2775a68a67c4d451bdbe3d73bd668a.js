462eb66a4a68e6c291cf6c5bf86c2c04746a3c92
Arpitkumar Soni <soniarpi@sheridancollege.ca>
2020-12-25 14:17:57 -0500

    Updates

---
+++ precache-manifest.5a2775a68a67c4d451bdbe3d73bd668a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd74d5fc24dfd3d84c6e2f4aa66c9646",
+    "url": "/GitHubFinder/index.html"
+  },
+  {
+    "revision": "465e2bed0648b64f9102",
+    "url": "/GitHubFinder/static/css/main.4062d4ae.chunk.css"
+  },
+  {
+    "revision": "c946ef54cbf1d990bef5",
+    "url": "/GitHubFinder/static/js/2.1d1db64b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GitHubFinder/static/js/2.1d1db64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465e2bed0648b64f9102",
+    "url": "/GitHubFinder/static/js/main.18937890.chunk.js"
+  },
+  {
+    "revision": "ff7dbef5ad605bc5eb8a",
+    "url": "/GitHubFinder/static/js/runtime-main.08ee12f2.js"
+  }
+]);
